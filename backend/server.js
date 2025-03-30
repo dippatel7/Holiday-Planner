0a1a90ff23faa77817efb91311c68bba4f978494
dippatel7 <74584591+dippatel7@users.noreply.github.com>
2025-03-30 23:14:54 +1100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
 
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
-const mongoose = require('mongoose');
-mongoose.set('strictQuery', false);
+
 dotenv.config();
 
 
@@ -13,7 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/tasks', require('./routes/taskRoutes'));
+app.use('/api/holidays', require('./routes/holidayRoutes'));
 
 // Export the app object for testing
 if (require.main === module) {
